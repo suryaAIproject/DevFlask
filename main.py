e08c7afec5711fcb1dad83bda7e5f281b67356ac
surya <mail4suryadip@gmail.com>
2023-04-25 23:29:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+app.run(debug=True)
